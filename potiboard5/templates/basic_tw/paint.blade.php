4307b1e475a271adae248d69d4144347097f1705
satopian <satopian@mia.eek.jp>
2022-08-23 00:35:29 +0900

    描画時間　JavaScript

--- potiboard5/templates/basic_tw/paint.blade.php
+++ potiboard5/templates/basic_tw/paint.blade.php
@@ -411,12 +411,13 @@ if(DynamicColor) PaletteListSetColor();
 <div class="centering">
 <!--描画時間動的表示-->
 <div class="applet_painttime">
-<FORM NAME="watch">繪圖時間
-<INPUT SIZE="20" NAME="count">
-</FORM>
-<Script>
-	timerID=10;stime=new Date;function SetTimeCount(){now=new Date;s=Math.floor((now.getTime()-stime.getTime())/1E3);disp="";86400<=s&&(d=Math.floor(s/86400),disp+=d+"\u65e5",s-=86400*d);3600<=s&&(h=Math.floor(s/3600),disp+=h+"\u6642\u9593",s-=3600*h);60<=s&&(m=Math.floor(s/60),disp+=m+"\u5206",s-=60*m);document.watch.count.value=disp+s+"\u79d2";clearTimeout(timerID);timerID=setTimeout(function() { SetTimeCount(); }, 250);}SetTimeCount();
-	</Script>
+<form name="watch">繪圖時間
+<input SIZE="20" NAME="count">
+</form>
+<script>
+	timerID=10;stime=new Date;function SetTimeCount(){now=new Date;s=Math.floor((now.getTime()-stime.getTime())/1E3);disp="";86400<=s&&(d=Math.floor(s/86400),disp+=d+"\u65e5",s-=86400*d);3600<=s&&(h=Math.floor(s/3600),disp+=h+"\u6642\u9593",s-=3600*h);60<=s&&(m=Math.floor(s/60),disp+=m+"\u5206",s-=60*m);document.watch.count.value=disp+s+"\u79d2";clearTimeout(timerID);timerID=setTimeout(function() { SetTimeCount(); }, 250);};
+	document.addEventListener('DOMContentLoaded',SetTimeCount,false);
+</script>
 	</div>
 <!--描画時間動的表示 ここまで-->
 <p>@if($anime) ★繪圖過程紀錄中★@endif</p>
