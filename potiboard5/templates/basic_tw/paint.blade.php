3188a12629eced4843b97cded69b5aef159fe32f
satopian <satopian@mia.eek.jp>
2022-10-10 21:34:29 +0900

    CheerpJ Subresource Integrity

--- potiboard5/templates/basic_tw/paint.blade.php
+++ potiboard5/templates/basic_tw/paint.blade.php
@@ -97,19 +97,21 @@
 	@if(($paint_mode and !$useneo) or ($pch_mode and !$type_neo))
 <!-- Javaが使えるかどうか判定 -->
 <script>
-        function cheerpJLoad() {
-        var jEnabled = navigator.javaEnabled();
-        if(!jEnabled){
-            var sN = document.createElement("script");
-            sN.src = "{{$cheerpj_url}}";
-            var s0 = document.getElementsByTagName("script")[0];
-            s0.parentNode.insertBefore(sN, s0);
-            sN.addEventListener("load", function(){ cheerpjInit(); }, false);
-            }
-        }
-        window.addEventListener("load", function() { cheerpJLoad(); }, false);
+	function cheerpJLoad() {
+	var jEnabled = navigator.javaEnabled();
+	if(!jEnabled){
+		var sN = document.createElement("script");
+		sN.src = "{{$cheerpj_url}}";
+		sN.integrity="{{$cheerpj_hash}}";
+		sN.crossOrigin="anonymous";
+		var s0 = document.getElementsByTagName("script")[0];
+		s0.parentNode.insertBefore(sN, s0);
+		sN.addEventListener("load", function(){ cheerpjInit(); }, false);
+		}
+	}
+	window.addEventListener("load", function() { cheerpJLoad(); }, false);
 </script>
-	@endif
+@endif
 @endif
 <style id="for_mobile"></style>
 <script>
