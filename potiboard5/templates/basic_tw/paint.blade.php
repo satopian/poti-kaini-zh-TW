77321cd57c11a38f2b5f8c52968557ebf1728a45
satopian <satopian@mia.eek.jp>
2023-10-02 00:21:17 +0900

    v6.06.1

--- potiboard5/templates/basic_tw/paint.blade.php
+++ potiboard5/templates/basic_tw/paint.blade.php
@@ -541,7 +541,6 @@ name="pch" code="pch.PCHViewer.class" archive="PCHViewer.jar,PaintBBS.jar" width
 	<form action="{{$self}}" method="post">
 		<input type="hidden" name="mode" value="download">
 		<input type="hidden" name="no" value="{{$no}}">
-		<input type="hidden" name="pch_ext" value="{{$pch_ext}}">
 		<span class="input_disp_none"><input type="text" value="" autocomplete="username"></span>
 		<span class="nk">密碼<input type="password" name="pwd" value="" class="paint_password" autocomplete="current-password"></span>
 		<input type="submit" value="下載 {{$pch_ext}} 文件">
