8dfce35327e6fd4158dc23410ed52cc4f07a509e
satopian <satopian@mia.eek.jp>
2026-06-01 13:08:25 +0900

    Viewerの記述がデーターセットの幅と高さになっていなかったのを修正。

--- potiboard5/templates/basic_tw/paint.blade.php
+++ potiboard5/templates/basic_tw/paint.blade.php
@@ -703,7 +703,7 @@ if(DynamicColor) PaletteListSetColor();
 
 	<div class="appstage" style="width:{{$w}}px; height:{{$h}}px">
 	@if($type_neo)
-	<div class="neo-applet-pch" width="{{$w}}" height="{{$h}}"></div>
+	<div class="neo-applet-pch" data-width="{{$w}}" data-height="{{$h}}"></div>
 	<script>
 	Neo.param ={
 		pch:{
